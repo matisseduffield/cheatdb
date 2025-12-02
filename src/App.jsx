bedf1c20c1e3520ea95e9813a875a9d0be5e63ba
matisseduffield <matisseduffield@gmail.com>
2025-12-02 21:52:42 +1030

    Move particles behind game cards - change z-index from 99999 to 1

--- src/App.jsx
+++ src/App.jsx
@@ -218,7 +218,7 @@ const FallingStars = () => {
           width: 100%;
           height: 100%;
           pointer-events: none;
-          z-index: 99999;
+          z-index: 1;
           overflow: hidden;
         }
 
