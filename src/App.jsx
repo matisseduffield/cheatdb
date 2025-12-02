364a57ec43ee0f5b29cc5c3d1d39d120de95f35e
matisseduffield <matisseduffield@gmail.com>
2025-12-02 22:12:27 +1030

    Lower particle container z-index from 1 to 0 for proper stacking

--- src/App.jsx
+++ src/App.jsx
@@ -218,7 +218,7 @@ const FallingStars = () => {
           width: 100%;
           height: 100%;
           pointer-events: none;
-          z-index: 1;
+          z-index: 0;
           overflow: hidden;
         }
 
